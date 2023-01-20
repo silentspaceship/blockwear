4f558b344e90d786ce0afba58c1d92f62c9602f8
Lucas <lucasdearaujo@pm.me>
2023-01-20 16:36:06 -0300

    styling refinements

--- src/App.js
+++ src/App.js
@@ -5,26 +5,29 @@ function App() {
     {
       id: 1,
       title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      imageUrl:
+        "https://images.unsplash.com/photo-1635650804512-003e5ee6ccac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     },
     {
       id: 2,
       title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      imageUrl:
+        "https://images.pexels.com/photos/5416411/pexels-photo-5416411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
     {
       id: 3,
       title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      imageUrl:
+        "https://images.pexels.com/photos/1032110/pexels-photo-1032110.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
     {
       id: 4,
-      title: "womens",
+      title: "women",
       imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: "mens",
+      title: "men",
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
